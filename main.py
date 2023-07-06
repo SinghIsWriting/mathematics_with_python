86af0495e8562898f04b4df56bc0aa74e94d9b9d
Singh <sabhisheksignh343104@gmail.com>
2023-07-06 11:06:02 +0530

    main.py file added

---
+++ main.py
@@ -0,0 +1,16 @@
+
+options = '''[1]     GCD
+[2]     LCM
+[3]     Mode
+[4]     Factorial pf a number
+[5]     Factors of a number
+[6]     Divisor of a number
+[7]     Fibonacci series
+[8]     Sum of Series
+[9]     Play with Sets
+[10]    Distance between two points
+[11]    Solve Quadratic Equation
+'''
+
+print("\n\t************************ Welcome to Maths with Singh ***********************\n\n")
+print(options)
