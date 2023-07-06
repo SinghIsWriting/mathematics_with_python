8eba0c211f7c726cf1ad21edbcb7910720c8d35c
Singh <sabhisheksignh343104@gmail.com>
2023-07-06 14:53:50 +0530

    divisor.py file added and implemented in main.py

---
+++ divisor.py
@@ -0,0 +1,27 @@
+import colorama
+from colorama import Fore
+colorama.init(autoreset=True)
+
+# defining a function that takes a number and returns a list of factors
+def isDivisor(num, divisor):
+    if num%divisor == 0:
+        return True
+    else:
+        return False
+
+def show():
+    print(f"\n {Fore.CYAN}********** Check either a number divides another number "+"**********\n")
+    try:
+        num = int(input("Enter the number here: "))
+        div = int(input("Enter the divisor here: "))
+        if isDivisor(num, div):
+            print(f"{Fore.GREEN}Yes!")
+        else:
+            print(f"{Fore.RED}No!")
+        print()
+    except:
+        print(f"\n{Fore.RED}Invalid input!")
+        print("Usage: Enter a natural number.\n")
+
+if __name__ == "__main__":
+	show()
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import colorama
+from colorama import Fore
+colorama.init(autoreset=True)
 import gcd
 import lcm
 import mode
 import factorial
 import factors
+import divisor
 
 options = '''
 [1]     GCD of two numbers
@@ -10,7 +14,7 @@ options = '''
 [3]     Mode
 [4]     Factorial of a number
 [5]     Factors of a number
-[6]     Divisor of a number
+[6]     Check Divisor
 [7]     Fibonacci series
 [8]     Sum of Series
 [9]     Play with Sets
@@ -19,7 +23,7 @@ options = '''
 [12]    Exit
 '''
 
-print("\n\t***************** Welcome to Maths with Singh *****************\n\n")
+print("\n\t*****************", f"{Fore.BLUE}Welcome to Maths with Singh", "*****************\n\n")
 
 while(True):
     print(options)
@@ -34,6 +38,8 @@ while(True):
         factorial.show()
     elif userOpt == "5" or userOpt.lower() == "factors":
         factors.show()
+    elif userOpt == "6" or userOpt.lower() == "divisor":
+        divisor.show()
     elif userOpt == "12" or userOpt.lower() == "exit":
         print("\nThanks for using this script :)")
         break
